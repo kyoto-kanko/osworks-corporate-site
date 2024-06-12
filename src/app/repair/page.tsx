673e775e9905ce3c9f79901d5d098440cab1c0a9
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-13 00:26:58 +0900

    add: table

---
+++ src/app/repair/page.tsx
@@ -0,0 +1,191 @@
+import React from "react";
+import Image from "next/image";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const ComponentName = () => {
+  return (
+    <div className="p-8">
+      <div className="text-4xl text-center text-inherit">料金表</div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Processing and Repair</TableHead>
+            <TableHead>Price</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.invoice}>
+              <TableCell>{invoice.invoice}</TableCell>
+              <TableCell>{invoice.paymentStatus}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableCaption>材料費込み / ミット類は + ¥1,000</TableCaption>
+      </Table>
+      <ScrollArea className="w-auto whitespace-nowrap rounded-md border">
+        <div className="flex w-max space-x-4 p-4">
+          {works.map((artwork) => (
+            <figure key={artwork.artist} className="shrink-0">
+              <div className="overflow-hidden rounded-md">
+                <Image
+                  src={artwork.art}
+                  alt={`Photo by ${artwork.artist}`}
+                  className="aspect-[3/4] h-fit w-fit object-cover"
+                  width={300}
+                  height={400}
+                />
+              </div>
+              <figcaption className="pt-2 text-xs text-muted-foreground">
+                Photo by{" "}
+                <span className="font-semibold text-foreground">
+                  {artwork.artist}
+                </span>
+              </figcaption>
+            </figure>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Processing and Repair</TableHead>
+            <TableHead>Price</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.invoice}>
+              <TableCell>{invoice.invoice}</TableCell>
+              <TableCell>{invoice.paymentStatus}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableCaption>材料費込み / ミット類は + ¥1,000</TableCaption>
+      </Table>
+      <ScrollArea className="w-auto whitespace-nowrap rounded-md border">
+        <div className="flex w-max space-x-4 p-4">
+          {works.map((artwork) => (
+            <figure key={artwork.artist} className="shrink-0">
+              <div className="overflow-hidden rounded-md">
+                <Image
+                  src={artwork.art}
+                  alt={`Photo by ${artwork.artist}`}
+                  className="aspect-[3/4] h-fit w-fit object-cover"
+                  width={300}
+                  height={400}
+                />
+              </div>
+              <figcaption className="pt-2 text-xs text-muted-foreground">
+                Photo by{" "}
+                <span className="font-semibold text-foreground">
+                  {artwork.artist}
+                </span>
+              </figcaption>
+            </figure>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </div>
+  );
+};
+
+export interface Artwork {
+  artist: string;
+  art: string;
+}
+
+export const works: Artwork[] = [
+  {
+    artist: "Ornella Binni",
+    art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Tom Byrom",
+    art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Vladimir Malyavko",
+    art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Ornella Binni",
+    art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Tom Byrom",
+    art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Vladimir Malyavko",
+    art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Ornella Binni",
+    art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Tom Byrom",
+    art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Vladimir Malyavko",
+    art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Ornella Binni",
+    art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Tom Byrom",
+    art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
+  },
+  {
+    artist: "Vladimir Malyavko",
+    art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
+  },
+];
+
+const invoices = [
+  {
+    invoice: "クラブ磨き",
+    paymentStatus: "¥2,000",
+  },
+  {
+    invoice: "紐交換1箇所",
+    paymentStatus: "1,800",
+  },
+  {
+    invoice: "紐交換3ヶ所",
+    paymentStatus: "Unpaid",
+  },
+  {
+    invoice: "全紐交換【グラブ】",
+    paymentStatus: "Paid",
+  },
+  {
+    invoice: "全紐交換【ミット】",
+    paymentStatus: "Paid",
+  },
+  {
+    invoice: "型付け【スチーム】",
+    paymentStatus: "Pending",
+  },
+  {
+    invoice: "型付け【湯揉み】",
+    paymentStatus: "Unpaid",
+  },
+];
+
+export default ComponentName;
