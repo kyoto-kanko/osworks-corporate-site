ea27590e05c8d7197f293a7e8f0023491400b234
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-15 11:55:50 +0900

    feat: top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import Slideshow from "../components/Slideshow";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-[calc(100vh-96px)] bg-black">
+    <main>
       <div>
-        <h1 className="text-center text-4xl font-bold text-green-500">
-          Hello, world<span className="blinking-cursor">|</span>
-        </h1>
+        <Slideshow />
       </div>
     </main>
   );
