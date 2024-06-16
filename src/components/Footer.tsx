d4c62665c14b6f79a9825116608f879580596b62
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-16 18:32:02 +0900

    fix: add title

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,6 +40,7 @@ const Footer = () => {
                         <div className="p-8 h-full w-full">
                           <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3265.18272387588!2d135.96361017784068!3d35.077166972788255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60017409793d5a8f%3A0x443ed96a83ffac3e!2z44CSNTI0LTAwNjEg5ruL6LOA55yM5a6I5bGx5biC6LWk6YeO5LqV55S677yS77yW77yU!5e0!3m2!1sja!2sjp!4v1718207336041!5m2!1sja!2sjp"
+                            title="Google Maps"
                             className="w-full h-full"
                             loading="lazy"
                           ></iframe>
