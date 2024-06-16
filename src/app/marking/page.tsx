9547075a639f731f9b24fda6862ad81eb6ef0df7
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-16 23:59:10 +0900

    fix: page

--- src/app/marking/page.tsx
+++ src/app/marking/page.tsx
@@ -7,9 +7,7 @@ function Page() {
         <h1 className="text-5xl font-extrabold mb-6 text-black animate-pulse">
           Coming Soon
         </h1>
-        <p className="text-xl text-gray-800 mb-4">
-          ただいま準備中です。お待ちください。
-        </p>
+        <p className="text-xl text-gray-800 mb-4">ただいま準備中です。</p>
         <div className="mt-8 flex justify-center space-x-2">
           <div className="w-4 h-4 bg-black rounded-full animate-bounce"></div>
           <div className="w-4 h-4 bg-gray-800 rounded-full animate-bounce200"></div>
